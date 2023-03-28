0f35d65b147f8f98b876668c9ac4eb5878629040
r.zamalyutdinov <ruslan.nnov@mail.ru>
2023-03-28 13:27:46 +0300

    index map v1.0.0

---
+++ static/js/index.js
@@ -0,0 +1,138 @@
+const svg = d3.select('svg')
+    .on("dblclick", function () {
+        svg.transition().duration(700).call(zoom.transform, d3.zoomIdentity)
+    })
+    .style('transition', 'transform 0.3s ease-in-out');
+
+const projection = d3.geoMercator()
+    .scale(300)
+    .center([0, 0])
+    .translate([-50, 800]);
+
+const pathGenerator = d3.geoPath().projection(projection);
+
+const zoom = d3.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
+const g = svg.append("g");
+
+function zoomed(event) {
+    const {transform} = event;
+    g.attr("transform", transform);
+    g.attr("stroke-width", 1 / transform.k);
+
+    g.selectAll("circle")
+        .style('stroke-width', 1 / transform.k)
+        .attr('r', 5 / transform.k);
+}
+
+d3.json('static/json/ussr.json')
+    .then(data => {
+        const countries = topojson.feature(data, data.objects.countries)
+
+        g.selectAll('path')
+            .data(countries.features)
+            .enter().append('path')
+            .attr("fill", "#ccc")
+            .attr("stroke", "#333")
+            .call(zoom)
+            .on("dblclick.zoom", null)
+            .attr('d', d => pathGenerator(d));
+
+        // Добавляем точки на карту из файла .csv
+        d3.csv('static/csv/stations.csv').then(data => {
+
+            data.forEach(function (d) {
+                d.latitude = +d.latitude;
+                d.longitude = +d.longitude;
+            });
+
+            g.selectAll("circle")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("id", function (d) {
+                    return d.id;
+                })
+                .attr("cx", function (d) {
+                    return projection([d.longitude, d.latitude])[0];
+                })
+                .attr("cy", function (d) {
+                    return projection([d.longitude, d.latitude])[1];
+                })
+                .attr("r", 5)
+                .style("fill", "red")
+                .style("stroke", "black")
+                .style("stroke-width", 1)
+                .on("mouseover", function () {
+                    let size = d3.select(this).attr('r')
+
+                    d3.select(this)
+                        .style("fill", "lightgreen")
+                        .attr("r", size * 1.3);
+                })
+                .on("mouseout", function () {
+                    let size = d3.select(this).attr('r')
+
+                    d3.select(this)
+                        .style("fill", "red")
+                        .attr("r", size * 10/13);
+                })
+                .append("title")
+                .text(function (d) {
+                    return d.station_name + ", " + d.region;
+                });
+
+            createList(data);
+        });
+    });
+
+function createList(data) {
+    const list = d3.select("#list")
+        .append("ul");
+
+    list.selectAll("li")
+        .data(data)
+        .enter()
+        .append("li")
+        .attr("id", function (d) {
+            return d.id;
+        })
+        .text(function (d) {
+            return d.station_name + ', ' + d.region;
+        })
+        .on("mouseover", function (d) {
+            const listItem = d3.select(this)
+                .style("cursor", "pointer")
+                .style("background-color", "lightgray")
+                .style("font-weight", "bold");
+
+            let circle = g.selectAll("circle")
+                .filter(function (e) {
+                    return e.id === listItem.attr("id");
+                });
+
+            let size = circle.attr('r');
+
+            circle
+                .style("fill", "lightgreen")
+                .attr("r", size * 1.3);
+        })
+        .on("mouseout", function (d) {
+            const listItem = d3.select(this)
+                .style("background-color", "white")
+                .style("font-weight", "normal");
+
+            let circle = g.selectAll("circle")
+                .filter(function (e) {
+                    return e.id === listItem.attr("id");
+                });
+
+            let size = circle.attr('r');
+
+            circle
+                .style("fill", "red")
+                .attr("r", size * 10/13);
+        });
+}
