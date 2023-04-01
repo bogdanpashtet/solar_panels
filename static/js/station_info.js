b6d57b9eefeeef2118e26192e97256759a64c899
r.zamalyutdinov <ruslan.nnov@mail.ru>
2023-04-01 22:51:20 +0300

    station_info.js refractoring

--- static/js/station_info.js
+++ static/js/station_info.js
@@ -1,9 +1,10 @@
 const API_V1_PREFIX = '/api/v1'
 const MONTH = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
 
-// получаем информацию о станции
-let url = window.location.origin + API_V1_PREFIX + window.location.pathname;
+const request = new XMLHttpRequest();
 
+// ---------------------- ИНФОРМАЦИЯ О СТАНЦИИ ----------------------
+let url = window.location.origin + API_V1_PREFIX + window.location.pathname;
 
 fetch(url)
     .then(response => response.json())
@@ -19,93 +20,17 @@ fetch(url)
         }
     );
 
-// получаем альбедо
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'albedo';
-
-let albedo_values
-
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-            const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-            albedo_values = months.map(key => +data[0][key]);
-
-            const dataSet = {
-                labels: MONTH,
-                datasets: [
-                    {
-                        label: 'Альбедо',
-                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                        borderColor: 'rgba(255, 99, 132, 1)',
-                        borderWidth: 1,
-                        data: albedo_values,
-                    }
-                ]
-            };
-
-            const config = {
-                type: 'line',
-                data: dataSet,
-                options: {
-                    maintainAspectRatio: false,
-                    responsive: true,
-                    aspectRatio: 1.5, // ширина = высота * 1.5
-                }
-            };
-
-            const albedo = new Chart(
-                document.getElementById('albedo'),
-                config
-            )
-        }
-    );
-
-// ---------------------- ДИФФУЗНАЯ СЛОНЕЧНАЯ РАДИАЦИЯ ----------------------
-
-// Месячная
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'diffuse-monthly';
-
-const request = new XMLHttpRequest();
-request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
-request.send();
-
-let diffuse_month = [];
-
-if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-    diffuse_month = months.map(key => +data[0][key]);
-}
-
-// Суточная
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'diffuse-daily';
-request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
-request.send();
-
-let diffuse_daily = [];
-
-if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-    diffuse_daily = months.map(key => +data[0][key]);
-}
+// ---------------------- АЛЬБЕДО ----------------------
 
 let dataSet = {
     labels: MONTH,
     datasets: [
         {
-            label: 'Месячная',
+            label: 'Альбедо',
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
-            data: diffuse_month,
-        },
-        {
-            label: 'Суточная',
-            backgroundColor: 'rgba(54, 162, 235, 0.2)',
-            borderColor: 'rgba(54, 162, 235, 1)',
-            borderWidth: 1,
-            data: diffuse_daily,
+            data: createDataset('albedo'),
         }
     ]
 };
@@ -120,7 +45,44 @@ let config = {
     }
 };
 
-let diffuse = new Chart(
+new Chart(
+    document.getElementById('albedo'),
+    config
+)
+
+// ---------------------- ДИФФУЗНАЯ СЛОНЕЧНАЯ РАДИАЦИЯ ----------------------
+
+dataSet = {
+    labels: MONTH,
+    datasets: [
+        {
+            label: 'Месячная',
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+            data: createDataset('diffuse-monthly'),
+        },
+        {
+            label: 'Суточная',
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1,
+            data: createDataset('diffuse-daily'),
+        }
+    ]
+};
+
+config = {
+    type: 'line',
+    data: dataSet,
+    options: {
+        maintainAspectRatio: false,
+        responsive: true,
+        aspectRatio: 1.5, // ширина = высота * 1.5
+    }
+};
+
+new Chart(
     document.getElementById('diffuse'),
     config
 )
@@ -128,33 +90,6 @@ let diffuse = new Chart(
 
 // ---------------------- СУММАРНАЯ СЛОНЕЧНАЯ РАДИАЦИЯ ----------------------
 
-// Месячная
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'total-monthly';
-
-request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
-request.send();
-
-let total_month = [];
-
-if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-    total_month = months.map(key => +data[0][key]);
-}
-
-// Суточная
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'total-daily';
-request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
-request.send();
-
-let total_daily = [];
-
-if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-    total_daily = months.map(key => +data[0][key]);
-}
-
 dataSet = {
     labels: MONTH,
     datasets: [
@@ -163,14 +98,14 @@ dataSet = {
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
-            data: total_month,
+            data: createDataset('total-monthly'),
         },
         {
             label: 'Суточная',
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
-            data: total_daily,
+            data: createDataset('total-daily'),
         }
     ]
 };
@@ -185,40 +120,12 @@ config = {
     }
 };
 
-const total = new Chart(
+new Chart(
     document.getElementById('total'),
     config
 )
 
-
 // ---------------------- ПРЯМАЯ СЛОНЕЧНАЯ РАДИАЦИЯ ----------------------
-
-// Месячная
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'direct-monthly';
-
-request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
-request.send();
-
-let direct_month = [];
-
-if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-    direct_month = months.map(key => +data[0][key]);
-}
-
-// Суточная
-url = window.location.origin + API_V1_PREFIX + window.location.pathname + 'direct-daily';
-request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
-request.send();
-
-let direct_daily = [];
-
-if (request.status === 200) {
-    const data = JSON.parse(request.responseText);
-    const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
-    direct_daily = months.map(key => +data[0][key]);
-}
 
 dataSet = {
     labels: MONTH,
@@ -228,14 +135,14 @@ dataSet = {
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgba(255, 99, 132, 1)',
             borderWidth: 1,
-            data: direct_month,
+            data: createDataset('direct-monthly'),
         },
         {
             label: 'Суточная',
             backgroundColor: 'rgba(54, 162, 235, 0.2)',
             borderColor: 'rgba(54, 162, 235, 1)',
             borderWidth: 1,
-            data: direct_daily,
+            data: createDataset('direct-daily'),
         }
     ]
 };
@@ -250,7 +157,20 @@ config = {
     }
 };
 
-const direct = new Chart(
+new Chart(
     document.getElementById('direct'),
     config
 )
+
+function createDataset(path) {
+    url = window.location.origin + API_V1_PREFIX + window.location.pathname + path;
+
+    request.open('GET', url, false);  // третий аргумент - false для синхронного запроса
+    request.send();
+
+    if (request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        const months = Object.keys(data[0]).filter(key => key.startsWith("month"));
+        return months.map(key => +data[0][key]);
+    }
+}
